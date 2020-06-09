8f8a291d2e74ec094efe3ac8583ee87e9357d1da
Carlos Santos <carlosmalltec@gmail.com>
2020-06-09 11:54:05 -0300

    Teste de components - finalizado.

--- web/src/views/Home/index.js
+++ web/src/views/Home/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
+//IMPORTACAO DE TODOS STYLEDS DA HOME
+import * as S from './styles';
+
+//COMPONENTS
+import Header from '../../components/Header';
+
 function Home() {
-  return <h1>Você está na home</h1>;
+
+  return (
+    <S.Container>
+      <Header />
+    </S.Container>
+  );
 }
 
 export default Home;
